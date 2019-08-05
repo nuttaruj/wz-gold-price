e44becca4497d3402e84b216455afddaa12facd5
Nuttaruj <nuttaruj@gmail.com>
2019-08-06 01:41:15 +0700

    fix

--- wz_gold_price.php
+++ wz_gold_price.php
@@ -1,22 +1,11 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://walnutztudio.com
- * @since             1.0.1
- * @package           Wz_gold_price
- *
  * @wordpress-plugin
  * Plugin Name:       Gold Price Widget
  * Plugin URI:        https://walnutztudio.com
  * Description:       Gold Price(THB) Widget for Wordpress
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            WalnutZtudio
  * Author URI:        https://walnutztudio.com
  * License:           GPL-2.0+
